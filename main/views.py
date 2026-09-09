32749191a015b24b81d60797340bf703e39c4f5b
Tania Ju <jutania93@gmail.com>
2026-09-09 16:45:22 +0700

    Tutorial 02: Implementasi MVT pada Django

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Tania Ju",
+        "npm": "2506608123",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Mahasiswa Sistem Informasi Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan pendidikan."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Tania Ju",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
